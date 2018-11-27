415047de62ff94e4040a94416cd49ba1d44e8350
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-11-26 20:27:46 -0800

    Setting up stress test

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-require('../client/header-sidebar-service/server/index.js');
-require('../client/instructors-service/server/index.js');
-require('../client/Student-Feedback/server/index.js');
-require('../client/students-also-bought-service/server/server.js');
+// require('../client/header-sidebar-service/server/index.js');
+// require('../client/instructors-service/server/index.js');
+// require('../client/Student-Feedback/server/index.js');
+// require('../client/students-also-bought-service/server/server.js');
 
 const express = require('express');
 const proxy = require('express-http-proxy');
 const path = require('path');
-
+const nr = require('newrelic');
 
 const PORT = 3005;
 const app = express();
@@ -16,6 +16,6 @@ app.listen(PORT, () => {
 app.use('/courses/:courseId', express.static(path.join(__dirname, '/../public')));
 
 app.get('/courses/:courseId/header', proxy('http://localhost:3003/'));
-app.get('/courses/:courseId/similarcourses', proxy('http://localhost:3004/'));
-app.get('/:Id/instructors', proxy('http://localhost:3002/'));
-app.get('/:courseId/reviews', proxy('http://localhost:3001/'));
+//app.get('/courses/:courseId/similarcourses', proxy('http://localhost:3004/'));
+//app.get('/:Id/instructors', proxy('http://localhost:3002/'));
+//app.get('/:courseId/reviews', proxy('http://localhost:3001/'));
