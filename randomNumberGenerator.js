2e40436c0da2bfd8ad08fc06f22c2e486226127f
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-11-27 15:16:25 -0800

    Add randomNumberGenerator.js for yml file

--- randomNumberGenerator.js
+++ randomNumberGenerator.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const generateRandomId = function(){
-  return Math.floor(Math.random() * 10000000))+1;
+  return Math.floor(Math.random() * 10000000)+1;
 }
 
 module.exports = generateRandomId;
